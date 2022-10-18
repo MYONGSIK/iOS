e5228cd2ac4136c0d452bb514e91d2bc484a834a
gom1n <alice436777@gmail.com>
2022-10-18 22:03:57 +0900

    [feat] 폰트 추가 (Noto Sans KR) #2

---
+++ MYONGSIK/domain/ViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MYONGSIK
+//
+//  Created by gomin on 2022/10/18.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
