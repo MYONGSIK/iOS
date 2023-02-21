9d271b8a6a9203c97e921a4ce818b9c104aa37ae
ryr0121 <ryr0121@gmail.com>
2023-02-21 18:20:10 +0900

    feat: (스플래시에서 자캠 선택 시) 식당 선택할 수 있는 화면 추가

---
+++ MYONGSIK/Source/Presentation/Main/ViewControllers/SelectRestaurantViewController.swift
@@ -0,0 +1,137 @@
+//
+//  SelectRestaurantViewController.swift
+//  MYONGSIK
+//
+//  Created by 김초원 on 2023/02/21.
+//
+
+import UIKit
+import SnapKit
+
+// MARK: 자연캠 식당 선택 화면
+class SelectRestaurantViewController: MainBaseViewController {
+    
+    let resInfo = [
+        ["교직원 식당", "09:00~17:30"],
+        ["생활관 식당", "08:00~17:30"],
+        ["학관 식당", "11:00~17:30"]
+    ]
+    
+    let adImageView = UIView().then{
+        $0.backgroundColor = .systemGray4
+        $0.layer.cornerRadius = 15
+    }
+    
+    var buttonTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpTableView(delegate: self)
+        setup()
+    }
+    
+    func setUpTableView(delegate: UITableViewDelegate & UITableViewDataSource) {
+        buttonTableView = UITableView().then{
+            $0.delegate = delegate
+            $0.dataSource = delegate
+            
+            // autoHeight
+            $0.rowHeight = UITableView.automaticDimension
+            $0.estimatedRowHeight = UITableView.automaticDimension
+            $0.separatorStyle = .none
+        }
+    }
+    
+    func setup() {
+        self.view.addSubview(adImageView)
+        self.view.addSubview(buttonTableView)
+        
+        adImageView.snp.makeConstraints {
+            $0.top.equalTo(super.navigationImgView.snp.bottom).inset(10)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().inset(15)
+            $0.height.equalTo(70)
+        }
+        buttonTableView.snp.makeConstraints {
+            $0.top.equalTo(adImageView.snp.bottom).offset(5)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().inset(15)
+            $0.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension SelectRestaurantViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.selectionStyle = .none
+        setButtonCell(cell, info: resInfo[indexPath.row])
+        print("setting cell")
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        // TODO: 선택된 식당의 학식 정보를 토대로 화면으로 전환 (MainVC) / 현재 화면 전환만 구현해둠
+        let mainVC = MainViewController()
+        self.navigationController?.pushViewController(mainVC, animated: true)
+    }
+}
+
+extension SelectRestaurantViewController {
+    private func setButtonCell(_ cell: UITableViewCell, info: [String]) {
+
+        let containerView = UIView().then {
+            $0.backgroundColor = .white
+            $0.layer.cornerRadius = 15
+            $0.layer.borderWidth = 0.1
+            
+            $0.layer.shadowColor = UIColor.gray.cgColor
+            $0.layer.masksToBounds = false
+            $0.layer.shadowOffset = CGSize(width: 0, height: 0)
+            $0.layer.shadowRadius = 4
+            $0.layer.shadowOpacity = 0.25
+        }
+        let restaurantNameLabel = UILabel().then {
+            $0.text = info[0]
+            $0.font = UIFont.NotoSansKR(size: 24, family: .Bold)
+            $0.textColor = .signatureBlue
+        }
+        let operatingTimeLabel = UILabel().then {
+            $0.font = UIFont.NotoSansKR(size: 16, family: .Regular)
+            $0.text = "운영시간 \(info[1])"
+            $0.textColor = .black
+        }
+        let intoButton = UIButton().then {
+            $0.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+            $0.tintColor = .signatureBlue
+        }
+        
+        cell.contentView.addSubview(containerView)
+        containerView.addSubview(restaurantNameLabel)
+        containerView.addSubview(operatingTimeLabel)
+        containerView.addSubview(intoButton)
+        
+        containerView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(15)
+            $0.bottom.leading.trailing.equalToSuperview().inset(5)
+        }
+        restaurantNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(50)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        operatingTimeLabel.snp.makeConstraints {
+            $0.leading.equalTo(restaurantNameLabel.snp.leading)
+            $0.top.equalTo(restaurantNameLabel.snp.bottom)
+            $0.bottom.equalToSuperview().inset(50)
+        }
+        intoButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(30)
+        }
+        
+    }
+}
--- MYONGSIK/Source/Presentation/Utils/TabBarViewController.swift
+++ MYONGSIK/Source/Presentation/Utils/TabBarViewController.swift
@@ -12,11 +12,12 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.tabBar.backgroundColor = .white
         
        // 인스턴스화
         let resVC = RestaurantMainViewController()
-        let mainVC = MainViewController()
+        let mainVC = isYonginCampus() ? SelectRestaurantViewController() :  MainViewController()
         let heartVC = HeartViewController()
         
         resVC.tabBarItem.image = UIImage.init(named: "res")
@@ -39,5 +40,12 @@ class TabBarViewController: UITabBarController {
         setViewControllers([nav1, nav2, nav3], animated: false)
         
         self.selectedIndex = 1
+    }
+    
+    func isYonginCampus() -> Bool {
+        if let userCampus = UserDefaults.standard.object(forKey: "userCampus") {
+            if userCampus as! String == Campus.yongin.rawValue { return true }
+        } 
+        return false
     }
 }
