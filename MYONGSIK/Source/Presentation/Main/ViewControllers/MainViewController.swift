0dbee34ebe3576f375f8af7dd02a88c972e85074
ryr0121 <ryr0121@gmail.com>
2023-02-26 18:50:02 +0900

    chore: 테스트 용 데이터 설정 코드 삭제

--- MYONGSIK/Source/Presentation/Main/ViewControllers/MainViewController.swift
+++ MYONGSIK/Source/Presentation/Main/ViewControllers/MainViewController.swift
@@ -564,10 +564,6 @@ extension MainViewController {
         
         endDay = Calendar.current.date(byAdding: .day, value: 4, to: startDay!)
         
-        // 임시 테스트용
-        startDay = Calendar.current.date(byAdding: .day, value: 1, to: today)
-        endDay = Calendar.current.date(byAdding: .day, value: 4, to: startDay!)
-        
         print("tablePageControl.currentPage - \(tablePageControl.currentPage)")
         print("start day - \(startDay)")
         print("end day - \(endDay)")
