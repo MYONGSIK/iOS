5fac933e9237b515f56f1837c5d6792586a2e01e
ryr0121 <ryr0121@gmail.com>
2023-02-26 21:27:40 +0900

    chore: 불필요한 출력문 삭제

--- MYONGSIK/Source/Presentation/Main/ViewControllers/MainViewController.swift
+++ MYONGSIK/Source/Presentation/Main/ViewControllers/MainViewController.swift
@@ -565,10 +565,6 @@ extension MainViewController {
 //        startDay = Calendar.current.date(byAdding: .day, value: 1, to: today)
 //        endDay = Calendar.current.date(byAdding: .day, value: 4, to: startDay!)
         
-        print("tablePageControl.currentPage - \(tablePageControl.currentPage)")
-        print("start day - \(startDay)")
-        print("end day - \(endDay)")
-        
         if isWeekend { titleLabel.text = "오늘의 학식  |  \(getTodayDataText(date: endDay!))" }
     }
     
