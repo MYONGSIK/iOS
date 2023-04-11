1b612c640992771b51c954fb8bc70af59ec7772d
ryr0121 <ryr0121@gmail.com>
2023-04-11 10:56:33 +0900

    fix: 맛집 경도/위도값 이슈 처리

--- MYONGSIK/Source/Domain/Entity/HeartListData.swift
+++ MYONGSIK/Source/Domain/Entity/HeartListData.swift
@@ -28,9 +28,35 @@ struct HeartModel: Codable {
     var code: String?
     var contact: String! = "전화번호없음"
     var distance: String?
-    var longitude: String?  // 경도, x
+    var longitude: String?  // 경도, x 
     var latitude: String?   // 위도, y
     var name: String?
     var phoneId: String?
     var urlAddress: String?
 }
+
+struct Heart: Codable {
+    var id: Int
+    var code: String?
+    var name: String?
+    var distance: String?
+    var category: String?
+    var address: String?
+    var contact: String?
+    var urlAddress: String?
+    var latitude: String?
+    var longitude: String?
+}
+
+//{
+//        "id": 222,
+//        "code": "14631333",
+//        "name": "베가보쌈",
+//        "distance": "131",
+//        "category": "음식점",
+//        "address": "서울 서대문구 거북골로 33-4",
+//        "contact": "02-372-8254",
+//        "urlAddress": "http://place.map.kakao.com/14631333",
+//        "latitude": "126.924013870389",
+//        "longitude": "37.5792516250279"
+//      },
