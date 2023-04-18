818e49b61d914ab6001787ef7e0f24ad0cae4ae8
ryr0121 <ryr0121@gmail.com>
2023-04-19 03:10:29 +0900

    feat: 위젯 디자인대로 UI 수정

--- DailyFoodWidget/DailyFoodWidget.swift
+++ DailyFoodWidget/DailyFoodWidget.swift
@@ -8,6 +8,13 @@
 import WidgetKit
 import SwiftUI
 
+// MARK: Constants
+struct OperatingTimeStr {
+    static let mccLanch = "중식 운영 시간 11:30~14:00"
+    static let mccDinner = "석식 운영 시간 17:30~19:00"
+}
+
+
 // MARK: api 통신을 위한 구조체 및 전역 상수 추가
 struct WidgetAPIModel: Decodable {
     let success: Bool?
@@ -87,7 +94,7 @@ struct FoodEntry: TimelineEntry {
         }
     }
     
-    func getFoodStr(type: String) -> String {
+    func getFoodsStr(type: String) -> String {
         var foodStr: String = ""
         mealData.forEach { data in
             if data.mealType == type {
@@ -98,6 +105,12 @@ struct FoodEntry: TimelineEntry {
         }
         return foodStr
     }
+    
+    func getTodayStr() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter.string(from: Date())
+    }
 }
 
 // MARK: Widget의 content를 보여주는 SwiftUI View
@@ -106,31 +119,163 @@ struct DailyFoodWidgetEntryView : View {
 
     var body: some View {
         VStack(alignment: .center, spacing: 10, content: {
-            Text("MCC식당").bold()
+            
+            /// 운영시간 & 식당명
+            HStack(content: {
+                Text(entry.getTodayStr())
+                    .bold()
+                    .foregroundColor(Color(uiColor: UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)))
+                    .font(.system(size: 11))
+                Spacer()
+                Text("MCC식당")
+                    .bold()
+                    .foregroundColor(Color(uiColor: UIColor(red: 101/255, green: 101/255, blue: 101/255, alpha: 1)))
+                    .font(.system(size: 11))
+                
+            }).padding(EdgeInsets(top: 20, leading: 30, bottom: 0, trailing: 30))
+            
 
+            /// 학식 종류명 & 학식 내용
             if entry.mealData.count > 0 {
-                ForEach(0..<entry.mealData.count, content: { idx in
-                    LazyVStack(alignment: .leading, content: {
-                        Text(entry.getFoodTypeStr(type: entry.mealData[idx].mealType ?? ""))
-                            .font(.body)
-                            .bold()
-                            .foregroundColor(.blue)
-                            .padding(EdgeInsets(top: 3, leading: 10, bottom: 3, trailing: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 14)
-                                    .stroke(Color.blue, lineWidth: 1.5)
-                            )
-                        
-                        Text(entry.getFoodStr(type: entry.mealData[idx].mealType ?? "정보를 찾을 수 없음")).font(.body).foregroundColor(.gray)
-                            .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
-                    }).padding(EdgeInsets(top: 0, leading: 25, bottom: 5, trailing: 25))
-
+                LazyVStack(alignment: .leading, content: {
+                    
+                    /// 중식
+                    ForEach(0..<entry.mealData.count-1, content: { idx in
+                        LazyHStack(alignment: .top, content: {
+                            Text(entry.getFoodTypeStr(type: entry.mealData[idx].mealType ?? ""))
+                               .font(.system(size: 14))
+                               .bold()
+                               .foregroundColor(Color(uiColor: UIColor(red: 10/255, green: 69/255, blue: 202/255, alpha: 1)))
+                               .padding(EdgeInsets(top: 8, leading: 10, bottom: 0, trailing: 10))
+                            
+                            Image("separator")
+                                .padding(EdgeInsets(top: 7, leading: 0, bottom: 0, trailing: 0))
+                            
+                            LazyVStack(alignment: .leading, content: {
+                                
+                                Text(entry.getFoodsStr(type: entry.mealData[idx].mealType ?? "정보를 찾을 수 없음"))
+                                    .font(.system(size: 13))
+                                    .foregroundColor(.black)
+                                    .padding(EdgeInsets(top: 0, leading: -5, bottom: 1, trailing: 0))
+                                
+                                Text(OperatingTimeStr.mccLanch)
+                                    .font(.system(size: 8))
+                                    .foregroundColor(.gray)
+                                
+                            })
+                            .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                            .frame(width: 210, height: 83)
+                            
+                        })
+                        .frame(height: 83)
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                                
+                    })
                 })
+                .background(Color(uiColor: UIColor(red: 246/255, green: 247/255, blue: 250/255, alpha: 1)))
+                .frame(width: 300)
+                .cornerRadius(16)
+                
+                
+                /// 석식
+                LazyVStack(alignment: .leading, content: {
+                    
+                    LazyHStack(alignment: .top, content: {
+                        Text(entry.getFoodTypeStr(type: entry.mealData[entry.mealData.count-1].mealType ?? ""))
+                           .font(.system(size: 14))
+                           .bold()
+                           .foregroundColor(Color(uiColor: UIColor(red: 10/255, green: 69/255, blue: 202/255, alpha: 1)))
+                           .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 18))
+                        
+                        Image("separator")
+                            .padding(EdgeInsets(top: 7, leading: 0, bottom: 0, trailing: 0))
+                        
+                        LazyVStack(alignment: .leading, content: {
+                            
+                            Text(entry.getFoodsStr(type: entry.mealData[entry.mealData.count-1].mealType ?? "정보를 찾을 수 없음"))
+                                .font(.system(size: 13))
+                                .foregroundColor(.black)
+                                .padding(EdgeInsets(top: 0, leading: -5, bottom: 4, trailing: 0))
+                            
+                            Text(OperatingTimeStr.mccDinner)
+                                .font(.system(size: 8))
+                                .foregroundColor(.gray)
+                            
+                        })
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                        .frame(width: 210, height: 83)
+                        
+                    })
+                    .frame(height: 83)
+                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                    
+                })
+                .background(Color(uiColor: UIColor(red: 246/255, green: 247/255, blue: 250/255, alpha: 1)))
+                .frame(width: 300)
+                .cornerRadius(16)
+                
+            
             } else {
                 Text("금일 학생식당을 운영하지 않습니다.")
                     .bold()
-                    .foregroundColor(.gray)
+                    .foregroundColor(.blue)
             }
+               
+                // 원본
+//            if entry.mealData.count > 0 {
+//                LazyVStack(content: {
+//                ForEach(0..<entry.mealData.count, content: { idx in
+//                    LazyVStack(alignment: .leading, content: {
+//                        Text(entry.getFoodTypeStr(type: entry.mealData[idx].mealType ?? ""))
+//                            .font(.body)
+//                            .bold()
+//                            .foregroundColor(.blue)
+//                            .padding(EdgeInsets(top: 3, leading: 10, bottom: 3, trailing: 10))
+//                            .overlay(
+//                                RoundedRectangle(cornerRadius: 14)
+//                                    .stroke(Color.blue, lineWidth: 1.5)
+//                            )
+//
+//                        Text(entry.getFoodStr(type: entry.mealData[idx].mealType ?? "정보를 찾을 수 없음")).font(.body).foregroundColor(.gray)
+//                            .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
+//                    }).padding(EdgeInsets(top: 0, leading: 25, bottom: 5, trailing: 25))
+                    
+//
+//                        LazyHStack(alignment: .top, content: {
+//                            VStack(content: {
+//                                Text(entry.getFoodTypeStr(type: entry.mealData[idx].mealType ?? ""))
+//                                    .font(.system(size: 14))
+//                                    .bold()
+//                                    .foregroundColor(Color(uiColor: UIColor(red: 10/255, green: 69/255, blue: 202/255, alpha: 1)))
+//                            })
+////                            .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+//                                .background(Color.brown)
+//
+//                            Spacer()
+//                            LazyVStack(alignment: .leading, content: {
+//
+//                                Text("식단 내용 어쩌고 저쩌고 식단 내용 어쩌고 저쩌고 식단 내용 어쩌고 저쩌고")
+//                                    .font(.system(size: 14))
+//                                    .foregroundColor(.gray)
+//                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
+//
+//                                Text("운영시간 00:00 ~ 00:00")
+//                                    .font(.system(size: 8))
+//                                    .foregroundColor(.gray)
+//
+//                            }).padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+//                                .frame(width: 210)
+//                                .background(Color.red)
+//
+//                        }).background(Color.orange)
+
+//                })
+//
+//            } else {
+//                Text("금일 학생식당을 운영하지 않습니다.")
+//                    .bold()
+//                    .foregroundColor(.blue)
+//            }
             
         })
     }
