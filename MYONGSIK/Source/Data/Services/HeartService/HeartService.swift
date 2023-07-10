1e56135dedb886326715cda6f96e39d1c89a679f
chayoosang <yoo7969@naver.com>
2023-07-10 19:10:37 +0900

    refactor: mvvm 변환 초기 설정

---
+++ MYONGSIK/Source/Data/Services/HeartService/HeartService.swift
@@ -0,0 +1,22 @@
+//
+//  HeartService.swift
+//  MYONGSIK
+//
+//  Created by 유상 on 2023/06/05.
+//
+
+import Foundation
+
+class HeartService {
+    func getMyHeart() {
+        
+    }
+    
+    func postHeart() {
+        
+    }
+    
+    func deleteHeart() {
+        
+    }
+}
